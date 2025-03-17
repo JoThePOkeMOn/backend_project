ba4b6abb2b6a44cce19c7dab603e0573db12d31a
JoThePOkeMOn <joelmathewsamuel@gmail.com>
2025-03-18 00:15:43 +0530

    add fileupload util and multer

---
+++ src/utils/FileUpload.js
@@ -0,0 +1,25 @@
+import v2 from "cloudinary"
+import fs from "fs"
+
+ cloudinary.config({
+   cloud_name: process.env.CLOUDINARY_CLOUDNAME,
+   api_key: process.env.CLOUDINARY_API_KEY,
+   api_secret: process.env.CLOUDINARY_API_SECRET,
+ });
+
+ const fileUpload = async (LocalPathName)=>{
+   try {
+    if(!LocalPathName)  return new Error("Path not found")
+    const response = await v2.uploader.upload(LocalPathName,{
+        resource_type : "auto"
+    })
+
+    fs.unlinkSync(LocalPathName)
+    return response
+   } catch (error) {
+     fs.unlinkSync(LocalPathName);
+     return null
+   }
+ }
+
+ export {fileUpload}
