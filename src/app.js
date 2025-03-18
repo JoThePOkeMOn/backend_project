218da01a3a8053a38aecae281d96944c4a229360
JoThePOkeMOn <joelmathewsamuel@gmail.com>
2025-03-19 00:17:40 +0530

    setup user route config

--- src/app.js
+++ src/app.js
@@ -11,5 +11,10 @@ app.use(cors({
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true}))
 app.use(express.static("public"))
-app.use(cookieParser()) 
+app.use(cookieParser())
+
+import userRouter from "./routes/user.routes.js"
+
+app.use("/api/user",userRouter)
+
 export default app
